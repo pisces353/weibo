4fc502eaa6888d850418eb54a375c5117d69e2c1
vagrant <vagrant@homestead>
2019-10-26 17:42:18 +0000

    发布微博

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -28,6 +29,15 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user){
+            $user->activation_token = Str::random(10);
+        });
+    }
+
     /**
      * The attributes that should be cast to native types.
      *
@@ -41,5 +51,10 @@ class User extends Authenticatable
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
+    public function statues()
+    {
+        return $this->hasMany(Status::class);
     }
 }
