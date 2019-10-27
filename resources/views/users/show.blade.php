6484bb45ec40fc90d5df34d124ae1cd5fffeb293
vagrant <vagrant@homestead>
2019-10-27 07:58:27 +0000

    关注按钮

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,4 +1,4 @@
-extends('layouts.default')
+@extends('layouts.default')
 @section('title', $user->name)
 
 @section('content')
@@ -7,6 +7,11 @@ extends('layouts.default')
     <section class="user_info">
       @include('shared._user_info', ['user' => $user])
     </section>
+
+    @if (Auth::check())
+      @include('users._follow_form')
+    @endif
+
     <section class="stats mt-2">
       @include('shared._stats', ['user' => $user])
     </section>
